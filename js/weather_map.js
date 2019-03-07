bb1ef700ce9fde5ada09bf4f1816e1fb975c3110
Anwar Lopez <anwarlo247@gmail.com>
2019-03-07 11:55:03 -0600

    inserted value data for forecast and some styling for page

--- js/weather_map.js
+++ js/weather_map.js
@@ -16,31 +16,29 @@ console.log(dateObject.toString());
 
 $.get(darkSkyUrl).done(function (darkSkyObj) {
 
-    //day 0
-    console.log("DAY 0");
-    console.log("The current .temperature is " + darkSkyObj.currently.temperature);
-    console.log("The current .cloudCover is " + darkSkyObj.currently.cloudCover);
-    console.log("The current .humidity is " + darkSkyObj.currently.humidity);
-    console.log("The current .windSpeed is " + darkSkyObj.currently.windSpeed);
-    console.log("The current .pressure is " + darkSkyObj.currently.pressure);
+    for (var i = 0; i < 2; i++) {
+        //day 0
 
-    //day 1
-    console.log("DAY 1");
-    console.log("The day 1 high temp is " + darkSkyObj.daily.data[0].apparentTemperatureHigh);
-    console.log("The day 1 low temp is " + darkSkyObj.daily.data[0].apparentTemperatureLow);
-    console.log("The day 1 cloudCover is " + darkSkyObj.daily.data[0].cloudCover);
-    console.log("The day 1 humidity is " + darkSkyObj.daily.data[0].humidity);
-    console.log("The day 1 windSpeed is " + darkSkyObj.daily.data[0].windSpeed);
-    console.log("The day 1 pressure is " + darkSkyObj.daily.data[0].pressure);
+        $('#tempuratures').html("<div>" + "<strong>High Temp: </strong> " + Math.round(darkSkyObj.daily.data[0].apparentTemperatureHigh) + "&#176" + " "+ "<strong>Low Temp: </strong>" + Math.round(darkSkyObj.daily.data[0].apparentTemperatureLow) + "&#176" + "</div>");
+        $('#clouds').html("<div>" + "<strong>Clouds: </strong>" + darkSkyObj.daily.data[0].icon.split("-").join(" ") + "</div>");
+        $('#humidity').html("<div>" + "<strong>Humidity: </strong>" + (darkSkyObj.daily.data[0].humidity * 100).toFixed(0)+ "%" + "</div>");
+        $('#wind').html("<div>" + "<strong>Wind: </strong>" + darkSkyObj.daily.data[0].windSpeed + "<strong> MPH</strong>" + "</div>");
+        $('#pressure').html("<div>" + "<strong>Pressure: </strong>" + darkSkyObj.daily.data[0].pressure + "</div>");
 
-    //day 2
-    console.log("DAY 2");
-    console.log("The day 2 high temp is " + darkSkyObj.daily.data[1].apparentTemperatureHigh);
-    console.log("The day 2 low temp is " + darkSkyObj.daily.data[1].apparentTemperatureLow);
-    console.log("The day 2 cloudCover is " + darkSkyObj.daily.data[1].cloudCover);
-    console.log("The day 2 humidity is " + darkSkyObj.daily.data[1].humidity);
-    console.log("The day 2 windSpeed is " + darkSkyObj.daily.data[1].windSpeed);
-    console.log("The day 2 pressure is " + darkSkyObj.daily.data[1].pressure);
+        //day 1
+        $('#tempuratures2').html("<div>" + "<strong>High Temp: </strong> " + Math.round(darkSkyObj.daily.data[1].apparentTemperatureHigh) + "&#176" + " "+ "<strong>Low Temp: </strong>" + Math.round(darkSkyObj.daily.data[1].apparentTemperatureLow) + "&#176" + "</div>");
+        $('#clouds2').html("<div>" + "<strong>Clouds: </strong>" + darkSkyObj.daily.data[1].icon.split("-").join(" ") + "</div>");
+        $('#humidity2').html("<div>" + "<strong>Humidity: </strong>" + (darkSkyObj.daily.data[1].humidity * 100).toFixed(0)+ "%" + "</div>");
+        $('#wind2').html("<div>" + "<strong>Wind: </strong>" + darkSkyObj.daily.data[1].windSpeed + "<strong> MPH</strong>" + "</div>");
+        $('#pressure2').html("<div>" + "<strong>Pressure: </strong>" + darkSkyObj.daily.data[1].pressure + "</div>");
+
+        //day 2
+        $('#tempuratures3').html("<div>" + "<strong>High Temp: </strong> " + Math.round(darkSkyObj.daily.data[2].apparentTemperatureHigh) + "&#176" + " "+ "<strong>Low Temp: </strong>" + Math.round(darkSkyObj.daily.data[2].apparentTemperatureLow) + "&#176" + "</div>");
+        $('#clouds3').html("<div>" + "<strong>Clouds: </strong>" + darkSkyObj.daily.data[2].icon.split("-").join(" ") + "</div>");
+        $('#humidity3').html("<div>" + "<strong>Humidity: </strong>" + (darkSkyObj.daily.data[2].humidity * 100).toFixed(0)+ "%" + "</div>");
+        $('#wind3').html("<div>" + "<strong>Wind: </strong>" + darkSkyObj.daily.data[2].windSpeed + "<strong> MPH</strong>" + "</div>");
+        $('#pressure3').html("<div>" + "<strong>Pressure: </strong>" + darkSkyObj.daily.data[2].pressure + "</div>");
+    }
 
 
 
